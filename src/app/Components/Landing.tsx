82206891eade4c4e2ca737fe2e90ebef91877310
samip-sharma <samip.sharma963@gmail.com>
2023-08-10 15:50:29 -0600

    landing page dark theme

--- src/app/Components/Landing.tsx
+++ src/app/Components/Landing.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import SvgLaptopMan from "./svgLaptopMan";
+import SvgLaptopMan from "./SvgLaptopMan";
 import AnimatedTypingComponent from "./AnimatedTypingComponent";
 import ChatBot from "./ChatBot";
 
