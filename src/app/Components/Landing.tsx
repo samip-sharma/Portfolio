c23ae4a22a4db9a3d15c1e61b319b531337436d6
samip-sharma <samip.sharma963@gmail.com>
2023-08-15 16:53:03 -0400

    wip cleanup

--- src/app/Components/Landing.tsx
+++ src/app/Components/Landing.tsx
@@ -7,7 +7,7 @@ import ChatBot from "./ChatBot";
 const Landing: React.FC = () => (
 	<div className="relative flex h-screen w-full flex-row justify-center bg-[rgb(231,138,61)] max-sm:flex-col">
 		<SvgLaptopMan />
-		<div className="tweenBackground flex flex-col items-center justify-center gap-10  dark:bg-black">
+		<div className="tweenBackground flex flex-col items-center gap-10  dark:bg-black">
 			<div className="max-sm:hidden">
 				<AnimatedTypingComponent />
 			</div>
